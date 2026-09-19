ff87c9d19fb4a0602f4ffd57c2c97cb572f3cb52
PINAKI DAS <pinakidas155@gmail.com>
2026-09-19 22:03:41 +0530

    Update settings.py

--- khata/settings.py
+++ khata/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-123456'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
